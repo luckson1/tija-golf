2e68a577701f54e90fd294afed2fe4b1170a1d5a
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-12-27 05:10:14 +0300

    remove print messages

--- src/utils.ts
+++ src/utils.ts
@@ -9,8 +9,6 @@ export const supabaseClient =  (  process.env.SUPABASE_URL &&
 export async function getUser (token:string) {
     const supabase=supabaseClient
 const userData= await supabase?.auth?.getUser(token)
-console.log(token, "token")
-console.log("user" , userData )
 const usersId=userData?.data.user?.id
 return usersId
 }
