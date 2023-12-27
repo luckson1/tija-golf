f66bc05a4b263fa86ab6e00a82503ef983acfaf2
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-12-27 05:36:29 +0300

    debugg

--- src/controllers/profileControllers.ts
+++ src/controllers/profileControllers.ts
@@ -21,6 +21,7 @@ const prisma = new PrismaClient();
 export const createProfile = async (req: Request, res: Response) => {
   try {
     const token=req.headers.authorization;
+    console.log(req.body, "body")
     if(!token) return   res.status(403).send('Forbidden');
     const usersId=await getUser(token)
     if (!usersId)  return   res.status(401).send('Unauthorised');
