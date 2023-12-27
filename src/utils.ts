07e48160fe6039f55fb253e7129a42429aa24400
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-12-27 04:39:35 +0300

    debugggin

--- src/utils.ts
+++ src/utils.ts
@@ -10,7 +10,7 @@ export async function getUser (token:string) {
     const supabase=supabaseClient
 const userData= await supabase?.auth?.getUser(token)
 console.log(token, "token")
-console.log("user" , userData?.data )
+console.log("user" , userData )
 const usersId=userData?.data.user?.id
 return usersId
 }
