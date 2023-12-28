0778faf1efb94b737742630e1cfe1bccc032d33a
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-12-29 00:08:16 +0300

    right order

--- src/routes/booking.ts
+++ src/routes/booking.ts
@@ -1,15 +1,16 @@
 // routes/bookings.ts
 import express from 'express';
-import { createBooking, getAllBookings, getBooking, getUpcomingActivities, updateBooking, getTeeBookings } from '../controllers/bookingControllers';
+import { createBooking, getAllBookings, getBooking, getTeeBookings, getUpcomingActivities, updateBooking } from '../controllers/bookingControllers';
 
 
 const bookingsRoute = express.Router();
 
 bookingsRoute.post('/',  createBooking);
-bookingsRoute.get('/',  getAllBookings);
+bookingsRoute.get('/upcoming', getUpcomingActivities);
+bookingsRoute.get('/tee', getTeeBookings);
 bookingsRoute.get('/:id',  getBooking);
 bookingsRoute.put('/:id',  updateBooking);
-bookingsRoute.get('/upcoming', getUpcomingActivities);
-bookingsRoute.get('/tees', getTeeBookings);
+bookingsRoute.get('/',  getAllBookings);
+
 
 export default bookingsRoute;
