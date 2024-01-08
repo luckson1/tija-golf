548bbdef3374c4143682e966e6cdc825a6c01f8d
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2024-01-08 19:31:13 +0300

    formatted board

--- src/controllers/leadershipboard.ts
+++ src/controllers/leadershipboard.ts
@@ -4,13 +4,34 @@ const prisma = new PrismaClient();
 export const getLatestBoard = async (req:Request, res:Response) => {
     try {
         const board = await prisma.leaderBoard.findFirst({
+            select: {
+                LeaderBoardPoint: {
+                    select: {
+                        points: true,
+                        user: {
+                            select: {
+                               Profile: {
+                                select: {
+                                    name: true
+                                }
+                               } 
+                            }
+                        }
+                    },
+                    orderBy: {
+                        points: 'asc'
+                    }
+                }
+            },
             orderBy: {
                 date: "desc"
             }
         });
 
         if (board) {
-            res.json(board);
+            const formattedBoard=board.LeaderBoardPoint.map(b=> ({points:b.points, user: b.user.Profile?.name}))
+            
+            res.json(formattedBoard);
         } else {
             res.status(404).send("Leaderboard not found");
         }
