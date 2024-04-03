3188a0e5bafa00a68351ed81fa1e7214c3a8744a
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2024-04-03 15:06:25 +0300

    mpesa payments

--- src/routes/payments.ts
+++ src/routes/payments.ts
@@ -1,13 +1,16 @@
-
-import express from 'express';
-import { encriptPayment, simulation, webHookReq } from '../controllers/payments';
-
+import express from "express";
+import {
+  encriptPayment,
+  mpesaWebHookReq,
+  simulation,
+  webHookReq,
+} from "../controllers/payments";
 
 const paymentRoute = express.Router();
 
-paymentRoute.post('/encrypt', encriptPayment);
-paymentRoute.post('/webhook', webHookReq);
-paymentRoute.post('/simulate', simulation);
-
+paymentRoute.post("/encrypt", encriptPayment);
+paymentRoute.post("/webhook", webHookReq);
+paymentRoute.post("/m-pesa-webhook", mpesaWebHookReq);
+paymentRoute.post("/simulate", simulation);
 
 export default paymentRoute;
