5290f423896caeb524772a305ddf57a2d633424b
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2024-04-08 11:45:51 +0300

    chores: format

--- src/controllers/attachaments.ts
+++ src/controllers/attachaments.ts
@@ -1,52 +1,56 @@
-import  fs  from 'fs'
+import fs from "fs";
 
-import {BlobServiceClient} from "@azure/storage-blob"
+import { BlobServiceClient } from "@azure/storage-blob";
 import { Request, Response } from "express";
-import { PrismaClient } from '@prisma/client';
-import { z } from 'zod';
-import { getUser } from '../utils';
+import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
+import { getUser } from "../utils";
 const prisma = new PrismaClient();
 const ProfileImageSchema = z.object({
-  id:z.string()
-  });
-export const uploadFileToAzure = async (filePath:string, fileName:string) => {
-try {
-  const AZURE_STORAGE_CONNECTION_STRING = process.env.APPSETTING_AZURE_STORAGE_CONNECTION_STRING;
-  const containerName = 'tijaeprocurementdev';
-  if (!AZURE_STORAGE_CONNECTION_STRING ) throw new Error("AZURE_STORAGE_CONNECTION_STRING needed")
-  const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
-  const containerClient = blobServiceClient.getContainerClient(containerName);
+  id: z.string(),
+});
+export const uploadFileToAzure = async (filePath: string, fileName: string) => {
+  try {
+    const AZURE_STORAGE_CONNECTION_STRING =
+      process.env.APPSETTING_AZURE_STORAGE_CONNECTION_STRING;
+    const containerName = "tijaeprocurementdev";
+    if (!AZURE_STORAGE_CONNECTION_STRING)
+      throw new Error("AZURE_STORAGE_CONNECTION_STRING needed");
+    const blobServiceClient = BlobServiceClient.fromConnectionString(
+      AZURE_STORAGE_CONNECTION_STRING
+    );
+    const containerClient = blobServiceClient.getContainerClient(containerName);
 
     const blobClient = containerClient.getBlockBlobClient(fileName);
     const uploadResponse = await blobClient.uploadFile(filePath);
 
-    console.log(blobClient.url)
+    console.log(blobClient.url);
     return blobClient.url;
-} catch (error) {
-  console.log( "errror", error)
-}
+  } catch (error) {
+    console.log("errror", error);
+  }
 };
 
-export const upload =async (req: Request, res: Response) => {
+export const upload = async (req: Request, res: Response) => {
   try {
     const token = req.headers.authorization;
     if (!token) return res.status(403).send("Forbidden");
     const usersId = await getUser(token);
     if (!usersId) return res.status(401).send("Unauthorised");
-    if (!req.file) return res.status(400).send('No file uploaded.');
-    const {id} = ProfileImageSchema.parse(req.body);
+    if (!req.file) return res.status(400).send("No file uploaded.");
+    const { id } = ProfileImageSchema.parse(req.body);
 
     const url = await uploadFileToAzure(req?.file?.path, req?.file?.filename);
-    console.log(url)
+    console.log(url);
     fs.unlinkSync(req?.file?.path); // Remove the file from the server after upload
-    const profileImage= await prisma.profile.update({
+    const profileImage = await prisma.profile.update({
       where: {
-id
+        id,
       },
       data: {
-        image:url
-      }
-    })
+        image: url,
+      },
+    });
 
     res.json({ url });
   } catch (error) {
@@ -54,7 +58,6 @@ id
     if (error instanceof z.ZodError) {
       return res.status(400).json(error.errors);
     }
-    res.status(500).send(`Error uploading file to Azure Storage.  ${error}` );
+    res.status(500).send(`Error uploading file to Azure Storage.  ${error}`);
   }
-  
-}
+};
