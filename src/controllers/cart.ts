2d1db8bc1db889129e81168ec6b89805c6599599
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2024-04-08 15:50:33 +0300

    fix: cart apis

--- src/controllers/cart.ts
+++ src/controllers/cart.ts
@@ -1,13 +1,13 @@
 import { z } from "zod";
 
 export const CartCreateSchema = z.object({
-  userId: z.string(),
   items: z.array(
     z.object({
       productId: z.string(),
       name: z.string(),
       price: z.number(),
       quantity: z.number().int().nonnegative(),
+      src: z.string().optional(),
     })
   ),
 });
@@ -32,6 +32,7 @@ export const createCart = async (req: Request, res: Response) => {
   if (!usersId) return res.status(401).send("Unauthorised");
   try {
     const { items } = CartCreateSchema.parse(req.body);
+
     const total = items.reduce(
       (sum, item) => sum + item.price * item.quantity,
       0
@@ -42,7 +43,12 @@ export const createCart = async (req: Request, res: Response) => {
           usersId,
           total,
           items: {
-            create: items,
+            create: items.map((item) => ({
+              productId: item.productId,
+              name: item.name,
+              price: item.price,
+              quantity: item.quantity,
+            })),
           },
         },
       });
@@ -86,29 +92,31 @@ export const updateCart = async (req: Request, res: Response) => {
       (sum, item) => sum + item.price * item.quantity,
       0
     );
-    // First, delete any existing items related to the cart
-    await prisma.shoppingItem.deleteMany({
-      where: {
-        cartId: id,
-      },
-    });
-
-    // Then, update the cart with the new total and create new items
-    const cart = await prisma.cart.update({
-      where: { id },
-      data: {
-        total,
-        // Use the 'create' operation within 'items' to add new items
-        items: {
-          create: items.map((item) => ({
-            productId: item.productId,
-            quantity: item.quantity,
-            name: item.name,
-            price: item.price,
-            cartId: id, // Ensure you set the correct relation field for cartId
-          })),
+    const cart = await prisma.$transaction(async (prisma) => {
+      // First, delete any existing items related to the cart
+      await prisma.shoppingItem.deleteMany({
+        where: {
+          cartId: id,
         },
-      },
+      });
+
+      // Then, update the cart with the new total and create new items
+      return prisma.cart.update({
+        where: { id },
+        data: {
+          total,
+          // Use the 'create' operation within 'items' to add new items
+          items: {
+            create: items.map((item) => ({
+              productId: item.productId,
+              quantity: item.quantity,
+              name: item.name,
+              price: item.price,
+              cartId: id, // Ensure you set the correct relation field for cartId
+            })),
+          },
+        },
+      });
     });
 
     res.status(200).json(cart);
