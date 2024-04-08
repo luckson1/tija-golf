b6907e31b3e6717d4335da139e16d236edf0d93f
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2024-04-08 17:16:32 +0300

    debuging auth

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
+console.log(
+  "url",
+  process.env.APPSETTING_SUPABASE_URL,
+  "key",
+  process.env.APPSETTING_SUPABASE_SERVICE_ROLE_KEY
+);
 export const supabaseClient =
   process.env.APPSETTING_SUPABASE_URL &&
   process.env.APPSETTING_SUPABASE_SERVICE_ROLE_KEY
@@ -10,7 +16,7 @@ export const supabaseClient =
 
 export async function getUser(token: string) {
   const supabase = supabaseClient;
-  console.log(supabase);
+
   const userData = await supabase?.auth?.getUser(token);
   const usersId = userData?.data.user?.id;
   return usersId;
