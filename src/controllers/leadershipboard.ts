ee65fefe13395be50f0bbe9d786b8eb8339684b8
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2024-04-30 13:28:20 +0300

    fix: dont throw error if no board

--- src/controllers/leadershipboard.ts
+++ src/controllers/leadershipboard.ts
@@ -1,41 +1,43 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 const prisma = new PrismaClient();
-export const getLatestBoard = async (req:Request, res:Response) => {
-    try {
-        const board = await prisma.leaderBoard.findFirst({
-            select: {
-             
-                LeaderBoardPoint: {
-                    select: {
-                        id:true,
-                        points: true,
-                       profile: {
-                        select: {
-                            name: true
-                        }
-                       }
-                    },
-                    orderBy: {
-                        points: 'asc'
-                    }
-                }
+export const getLatestBoard = async (req: Request, res: Response) => {
+  try {
+    const board = await prisma.leaderBoard.findFirst({
+      select: {
+        LeaderBoardPoint: {
+          select: {
+            id: true,
+            points: true,
+            profile: {
+              select: {
+                name: true,
+              },
             },
-            orderBy: {
-                date: "desc"
-            }
-        });
+          },
+          orderBy: {
+            points: "asc",
+          },
+        },
+      },
+      orderBy: {
+        date: "desc",
+      },
+    });
+    console.log(board);
+    if (board) {
+      const formattedBoard = board.LeaderBoardPoint.map((b) => ({
+        id: b.id,
+        points: b.points,
+        user: b.profile?.name,
+      }));
 
-        if (board) {
-       
-            const formattedBoard=board.LeaderBoardPoint.map(b=> ({id: b.id, points:b.points, user: b.profile?.name}))
-            
-            res.json(formattedBoard);
-        } else {
-            res.status(404).send("Leaderboard not found");
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("An error occurred while retrieving the leaderboard");
+      res.json(formattedBoard);
+    } else {
+      return [];
     }
-}
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while retrieving the leaderboard");
+  }
+};
