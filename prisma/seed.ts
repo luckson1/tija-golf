f77cad78b992e1de875415b6a2be289976f6287b
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2024-05-12 13:47:47 +0300

    fix: leaderboard

---
+++ prisma/seed.ts
@@ -0,0 +1,47 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+async function main() {
+  const data = [
+    { name: "Alice", points: 72 },
+    { name: "Bob", points: 68 },
+    { name: "Charlie", points: 70 },
+    { name: "David", points: 74 },
+    { name: "Eva", points: 69 },
+    { name: "Fiona", points: 65 },
+    { name: "George", points: 67 },
+    { name: "Hannah", points: 71 },
+    { name: "Ian", points: 73 },
+    { name: "Julia", points: 66 },
+  ];
+
+  await prisma.$transaction(async (db) => {
+    const leaderBoard = await db.leaderBoard.create({
+      data: {
+        date: new Date(),
+      },
+    });
+
+    console.log(`Start seeding LeaderBoardPoints ...`);
+    for (const item of data) {
+      const leaderBoardPoint = await db.leaderBoardPoint.create({
+        data: {
+          name: item.name,
+          points: item.points,
+          leaderBoardId: leaderBoard.id,
+        },
+      });
+      console.log(`Created LeaderBoardPoint with id: ${leaderBoardPoint.id}`);
+    }
+    console.log(`Seeding finished.`);
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
