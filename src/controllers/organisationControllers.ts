18f1f8e287ac088bb2cd1311501ba33f4495ebfa
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2024-05-22 19:31:56 +0300

    fix: debug

--- src/controllers/organisationControllers.ts
+++ src/controllers/organisationControllers.ts
@@ -67,6 +67,8 @@ export const getAllOrganisations = async (req: Request, res: Response) => {
     if (!token) return res.status(403).send("Forbidden");
     const usersId = await getUser(token);
 
+    console.log(usersId);
+
     if (!usersId) return res.status(401).send("Unauthorized");
     const organizations = await prisma.organization.findMany();
     res.json(organizations);
