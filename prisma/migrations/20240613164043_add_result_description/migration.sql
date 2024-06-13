c91784b3801ac7cb4565fad4589209aa58236940
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2024-06-13 19:52:44 +0300

    payments done

---
+++ prisma/migrations/20240613164043_add_result_description/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "auth"."Payment" ADD COLUMN     "resultDescription" TEXT;
--- src/controllers/payments.ts
+++ src/controllers/payments.ts
@@ -1,28 +1,11 @@
 import { z } from "zod";
-import checkoutEncrypt from "@cellulant/checkout_encryption";
 import { Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
+import { PaymentStatus, PrismaClient } from "@prisma/client";
+import base64 from "base-64"; // Ensure you have base-64 installed
+import { getUser } from "../utils";
+import { format } from "date-fns";
+// Adjust the path as necessary
 
-const apiSchema = z.object({
-  msisdn: z.string(),
-  account_number: z.string(),
-  country_code: z.string().length(3),
-  currency_code: z.string().length(3),
-  customer_first_name: z.string(),
-  customer_last_name: z.string(),
-  due_date: z.string(), // You may want to use a custom validation for date format
-  merchant_transaction_id: z.string(),
-  payment_option_code: z.string().optional(),
-  callback_url: z.string().url(),
-  pending_redirect_url: z.string().url().optional(),
-  request_amount: z.number(),
-  request_description: z.string(),
-  service_code: z.string(),
-  success_redirect_url: z.string().url(),
-  fail_redirect_url: z.string().url(),
-  language_code: z.enum(["fr", "en", "ar", "pt"]),
-  charge_beneficiaries: z.array(z.unknown()).optional(), // Replace z.unknown() with the appropriate schema for the objects in the array
-});
 interface WebhookResponse {
   Result: {
     ConversationID: string;
@@ -44,31 +27,30 @@ interface WebhookResponse {
     TransactionID: string;
   };
 }
-type Payment = z.infer<typeof apiSchema>;
+export interface PaymentStatusResponse {
+  ResponseCode: string;
+  ResponseDescription: string;
+  MerchantRequestID: string;
+  CheckoutRequestID: string;
+  ResultCode: string | number;
+  ResultDesc: string;
+}
+interface PaymentResponse {
+  MerchantRequestID: string;
+  CheckoutRequestID: string;
+  ResponseDescription: string;
+  ResponseCode: number;
+  CustomerMessage: string;
+}
+
+export const passKey = process.env.MPESA_PASS_KEY!;
+export const consumerKey = process.env.MPESA_CONSUMER_KEY!;
+export const consumerSecret = process.env.MPESA_CONSUMER_SECRET!;
+export const backendBaseUrl = process.env.BACKEND_BASE_URL!;
+export const businessShortCode = process.env.BUSINESSCODE ?? "174379"; // Add this line
 
 const prisma = new PrismaClient();
 
-// const apiSchema = z.object({
-//   merchantTransactionID: z.string(),
-//   requestAmount: z.string(), // Assuming this should be a string representing a numerical value
-//   currencyCode: z.string(),
-//   accountNumber: z.string(),
-//   serviceCode: z.string(),
-//   countryCode: z.string(),
-//   dueDate: z.string().optional(), // Assuming the date is in string format
-//   payerClientCode: z.string().optional(),
-//   requestDescription: z.string().optional(),
-//   languageCode: z.string().optional(),
-//   MSISDN: z.string(),
-//   customerFirstName: z.string(),
-//   customerLastName: z.string(),
-//   customerEmail: z.string().email(),
-//   successRedirectUrl: z.string(),
-//   pendingRedirectUrl: z.string().optional(),
-//   failRedirectUrl: z.string(),
-//   paymentWebhookUrl: z.string(),
-//   extraData: z.record(z.any()).optional(), // JSON is represented as a record of any type
-// });
 const callbackMetadataItemSchema = z.object({
   Name: z.string(),
   Value: z.union([z.number(), z.string()]).optional().nullable(), // Make Value optional
@@ -93,146 +75,6 @@ const webhookDataSchema = z.object({
   Body: bodySchema,
 });
 
-type WebhookData = z.infer<typeof webhookDataSchema>;
-const webhookRequestSchema = z.object({
-  request_amount: z.number(),
-  account_number: z.string(),
-  original_request_amount: z.number(),
-
-  amount_paid: z.number(),
-  service_charge_amount: z.number(),
-
-  service_code: z.string(),
-  request_status_code: z.enum([
-    "177",
-    "178",
-    "179",
-    "129",
-    "180",
-    "183",
-    "188",
-  ]),
-  request_status_description: z.string(),
-  msisdn: z.string(),
-  payments: z.array(z.unknown()), // Replace with actual schema for payment objects
-  failed_payments: z.array(z.unknown()), // Replace with actual schema for failed payment objects
-  status_date: z.string().optional(), // You may want to use a custom validation for date format
-  country_abbrev: z.string().length(2),
-  errors: z
-    .array(
-      z.object({
-        customer_name: z.string(),
-        account_number: z.string(),
-        cpp_transaction_id: z.string(),
-        currency_code: z.string().length(3),
-        payer_client_code: z.string(),
-        payer_client_name: z.string(),
-        amount_paid: z.number(),
-        service_code: z.string(),
-        date_payment_received: z.string(), // You may want to use a custom validation for date format
-        msisdn: z.string(),
-        payer_transaction_id: z.string(),
-        error: z.string(),
-      })
-    )
-    .optional(),
-});
-
-type PaymentResponse = z.infer<typeof webhookRequestSchema>;
-
-export const encriptPayment = async (req: Request, res: Response) => {
-  // const accessKey = process.env.APPSETTING_accessKey;
-  const IVKey = process.env.APPSETTING_IVKey;
-  const secretKey = process.env.APPSETTING_secretKey;
-  const algorithm = "aes-256-cbc";
-  const encryption =
-    IVKey && secretKey
-      ? new checkoutEncrypt.Encryption(IVKey, secretKey, algorithm)
-      : null;
-  try {
-    // Validate the input using Zod
-
-    const payloadObj = apiSchema.parse(req.body);
-    const toEncrypt = { ...payloadObj, due_date: undefined };
-    const payloadStr = JSON.stringify(toEncrypt);
-
-    var result = encryption?.encrypt(payloadStr);
-    res.status(201).json(result);
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      // If the error is a Zod validation error, send a bad request response
-      return res.status(400).json(error.errors);
-    }
-
-    // Handle other types of errors
-    res.status(500).send(error);
-  }
-};
-export const webHookReq = async (req: Request, res: Response) => {
-  try {
-    console.log("payment got here");
-    // Validate the input using Zod
-    console.log("request", req);
-    const payloadObj = webhookRequestSchema.parse(req.body);
-    const booking = await prisma.booking.update({
-      where: {
-        bookingRef: Number(payloadObj.account_number),
-      },
-
-      data: {
-        status:
-          payloadObj.request_status_code === "180"
-            ? "Rejected"
-            : payloadObj.request_status_code === "129"
-            ? "Expired"
-            : payloadObj.request_status_code === "177"
-            ? "Partial"
-            : payloadObj.request_status_code === "178"
-            ? "Completed"
-            : payloadObj.request_status_code === "179"
-            ? "Refunded"
-            : payloadObj.request_status_code === "183"
-            ? "Accepted"
-            : payloadObj.request_status_code === "188"
-            ? "Received"
-            : "Failed",
-      },
-    });
-    const payment = await prisma.payment.update({
-      where: {
-        bookingId: booking.id,
-      },
-      data: {
-        status:
-          payloadObj.request_status_code === "180"
-            ? "Rejected"
-            : payloadObj.request_status_code === "129"
-            ? "Expired"
-            : payloadObj.request_status_code === "177"
-            ? "Partial"
-            : payloadObj.request_status_code === "178"
-            ? "Completed"
-            : payloadObj.request_status_code === "179"
-            ? "Refunded"
-            : payloadObj.request_status_code === "183"
-            ? "Accepted"
-            : payloadObj.request_status_code === "188"
-            ? "Received"
-            : "Failed",
-      },
-    });
-    res.status(201).json(payment);
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      console.log("validation");
-      // If the error is a Zod validation error, send a bad request response
-      return res.status(400).json(error.errors);
-    }
-    console.log("others");
-    // Handle other types of errors
-    res.status(500).send(error);
-  }
-};
 export const mpesaWebHookReq = async (req: Request, res: Response) => {
   try {
     const invoiceNumber = req.params.invoiceNumber;
@@ -289,28 +131,6 @@ export const mpesaWebHookReq = async (req: Request, res: Response) => {
     res.status(500).send(error);
   }
 };
-export const getPaymentStatus = async (req: Request, res: Response) => {
-  const { invoiceNumber } = req.params;
-
-  try {
-    // Retrieve the payment record from the database
-    const payment = await prisma.payment.findUnique({
-      where: { invoiceNumber },
-    });
-    console.log("payment", payment);
-    // If payment doesn't exist, return pending
-    if (!payment) {
-      return res.status(200).json({ status: "Pending" });
-    }
-    console.log(payment.status);
-    // Respond with the payment status
-    return res.status(200).json({ status: payment.status });
-  } catch (error) {
-    // Log the error and return a 500 status code
-    console.error("Error fetching payment status:", error);
-    return res.status(500).json({ message: "Error fetching payment status" });
-  }
-};
 
 export const updatePaymentStatusFromWebhook = async (
   req: Request,
@@ -319,11 +139,11 @@ export const updatePaymentStatusFromWebhook = async (
   try {
     // Assuming the body of the request is the WebhookResponse
     const webhookResponse: WebhookResponse = req.body;
-await  prisma.webhookJson.create({
-  data: {
-    body:webhookResponse as any
-  }
-})
+    await prisma.webhookJson.create({
+      data: {
+        body: webhookResponse as any,
+      },
+    });
     const invoiceNumber = req.params.invoiceNumber;
     // Extract necessary data from the webhook response
 
@@ -385,6 +205,259 @@ await  prisma.webhookJson.create({
     }
     // Log and handle other types of errors
     console.error("Error updating payment status:", error);
+    return res.status(500).send(error);
+  }
+};
+
+const getBearerToken = async () => {
+  try {
+    const buffer = Buffer.from(`${consumerKey}:${consumerSecret}`);
+    const auth = `Basic ${buffer.toString("base64")}`;
+
+    const response = await fetch(
+      "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
+      {
+        method: "GET",
+        headers: {
+          Authorization: auth,
+        },
+      }
+    );
+
+    if (response.status !== 200) {
+      throw new Error("Something went wrong");
+    }
+
+    const data = await response.json();
+    return data.access_token;
+  } catch (error) {
+    console.error("Error fetching bearer token:", error);
+    throw error;
+  }
+};
+
+// Define validation schemas
+const sendPaymentRequestSchema = z.object({
+  amount: z.number(),
+  partyA: z.number(),
+  phoneNumber: z.number(),
+  transactionDesc: z.string(),
+  invoiceNumber: z.string(),
+});
+
+const checkPaymentStatusSchema = z.object({
+  invoiceNumber: z.string(),
+});
+
+/**
+ * @swagger
+ * /api/payments/send:
+ *   post:
+ *     summary: Send a payment request
+ *     tags: [Payments]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               amount:
+ *                 type: number
+ *               partyA:
+ *                 type: number
+ *               phoneNumber:
+ *                 type: number
+ *               transactionDesc:
+ *                 type: string
+ *               invoiceNumber:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Payment request sent successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
+export const sendPaymentRequest = async (req: Request, res: Response) => {
+  try {
+    const token = req.headers.authorization;
+    if (!token) return res.status(403).send("Forbidden");
+    const userId = await getUser(token);
+    if (!userId) return res.status(401).send("Unauthorized");
+
+    // Validate request body
+    const parsedBody = sendPaymentRequestSchema.safeParse(req.body);
+    if (!parsedBody.success) {
+      return res.status(400).json(parsedBody.error.errors);
+    }
+
+    const { amount, partyA, phoneNumber, transactionDesc, invoiceNumber } =
+      parsedBody.data;
+
+    const timestamp = format(new Date(), "yyyyMMddHHmmss");
+    const password = base64.encode(businessShortCode + passKey + timestamp);
+    const accessToken = await getBearerToken();
+    const callBackUrl = `${backendBaseUrl}/api/payments/webhook/mpesa/${invoiceNumber}`;
+
+    const response = await fetch(
+      "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({
+          BusinessShortCode: businessShortCode,
+          Password: password,
+          Timestamp: timestamp,
+          TransactionType: "CustomerPayBillOnline",
+          Amount: amount,
+          PartyA: partyA,
+          PartyB: businessShortCode,
+          PhoneNumber: phoneNumber,
+          CallBackURL: callBackUrl,
+          AccountReference: "TIJA GOLF LTD",
+          TransactionDesc: transactionDesc,
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      const errorMessage =
+        errorData.errorMessage || "Error occurred initializing payment";
+      console.error("Payment initialization failed:", errorMessage);
+      throw new Error(errorMessage);
+    }
+
+    const results: PaymentResponse = await response.json();
+    const paymentData = {
+      invoiceNumber,
+      amount: Number(amount),
+      checkoutRequestID: results.CheckoutRequestID,
+    };
+    await prisma.payment.upsert({
+      where: { invoiceNumber },
+      update: paymentData,
+      create: paymentData,
+    });
+    return res.status(200).json({ status: "Pending" });
+  } catch (error) {
+    console.error("Error sending payment request:", error);
+    return res.status(500).send(error);
+  }
+};
+
+/**
+ * @swagger
+ * /api/payments/check:
+ *   post:
+ *     summary: Check payment status
+ *     tags: [Payments]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               invoiceNumber:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Payment status checked successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
+export const checkPaymentStatus = async (req: Request, res: Response) => {
+  try {
+    const token = req.headers.authorization;
+    if (!token) return res.status(403).send("Forbidden");
+    const userId = await getUser(token);
+    if (!userId) return res.status(401).send("Unauthorized");
+
+    // Validate request body
+    const parsedBody = checkPaymentStatusSchema.safeParse(req.body);
+    if (!parsedBody.success) {
+      return res.status(400).json(parsedBody.error.errors);
+    }
+
+    const { invoiceNumber } = parsedBody.data;
+    const timestamp = format(new Date(), "yyyyMMddHHmmss");
+    const payment = await prisma.payment.findUnique({
+      where: { invoiceNumber },
+      select: { checkoutRequestID: true },
+    });
+    const checkoutRequestID = payment?.checkoutRequestID;
+    if (!checkoutRequestID)
+      return res.status(200).json({ status: PaymentStatus.Pending });
+    const password = base64.encode(businessShortCode + passKey + timestamp);
+    const accessToken = await getBearerToken();
+
+    const response = await fetch(
+      "https://sandbox.safaricom.co.ke/mpesa/stkpushquery/v1/query",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({
+          BusinessShortCode: businessShortCode,
+          Password: password,
+          Timestamp: timestamp,
+          CheckoutRequestID: checkoutRequestID,
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      const errorMessage =
+        errorData.errorMessage || "Error occurred checking payment status";
+      console.error("Payment status check failed:", errorMessage);
+      throw new Error(errorMessage);
+    }
+    const results: PaymentStatusResponse = await response.json();
+    const status =
+      results.ResultCode === "0" || results.ResultCode === 0
+        ? PaymentStatus.Completed
+        : PaymentStatus.Failed;
+    await prisma.payment.update({
+      where: {
+        invoiceNumber,
+      },
+      data: {
+        status,
+        resultDescription: results.ResultDesc,
+      },
+    });
+    return res.status(200).json({ status });
+  } catch (error) {
+    console.error("Error checking payment status:", error);
     return res.status(500).send(error);
   }
 };
