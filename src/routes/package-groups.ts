c865bf49c9bca2c3706f7c5766d20451b2bddee3
Jackson Ndiang'ui <jacksongathondu@gmail.com>
2024-09-18 23:39:14 +0300

    fix: package groups

--- src/routes/package-groups.ts
+++ src/routes/package-groups.ts
@@ -1,18 +1,18 @@
 import express from "express";
-import { 
-  getAllPackageGroups, 
-  getPackageGroup, 
-  createPackageGroup, 
-  updatePackageGroup, 
-  deletePackageGroup 
+import {
+  getAllPackageGroups,
+  getPackageGroup,
+  createPackageGroup,
+  updatePackageGroup,
+  deletePackageGroup,
 } from "../controllers/package-groups";
 
 const router = express.Router();
 
-router.get("/packageGroups", getAllPackageGroups);
-router.get("/packageGroups/:id", getPackageGroup);
-router.post("/packageGroups", createPackageGroup);
-router.put("/packageGroups/:id", updatePackageGroup);
-router.delete("/packageGroups/:id", deletePackageGroup);
+router.get("/", getAllPackageGroups);
+router.get("//:id", getPackageGroup);
+router.post("/", createPackageGroup);
+router.put("//:id", updatePackageGroup);
+router.delete("//:id", deletePackageGroup);
 
 export default router;
