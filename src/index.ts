f9ef822259241c47e5b23759c42dbd3e8222c049
William Muli <william.muli@alternateltd.com>
2024-09-19 06:39:26 +0000

    Updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ import { fetchData } from "./controllers/data";
 const app = express();
 
 // const PORT = process.env.PORT || 8080;
-const PORT = 8080;
+const PORT = 8080; // hard-coded to fix azure web app service port issues
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
